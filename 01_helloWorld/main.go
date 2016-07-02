ad34aafce9f928262cd624cc853f2aba9d053cc5
Almazi <navilalmazi@gmail.com>
2016-07-02 23:51:20 +0600

    Added numeral system and loop codes

---
+++ 01_helloWorld/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Love is love is love is love is love, and love cannot be killed or swept aside.")
+	i := 3
+	fmt.Println("%d", i)
+
+}
---
+++ 02_numeralSystem/03_Hexadecimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	//fmt.Printf("%d - %b - %x", 42, 42, 42)
+	fmt.Printf("%d - %b - %#X", 42, 42, 42)
+}
